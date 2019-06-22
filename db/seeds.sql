c8c6bec0de70dcdda2002090cb3e8269eca774d2
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-06-21 23:12:56 -0600

    updated most files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
     
-INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Steakburger');
-INSERT INTO burgers (burger_name) VALUES ('Turkeyburger');
-INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger'), ('Hamburger'), ('Veggie Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steak burger', true), ('Mushroom Burger', false), ('Whopper', true);
--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+//adding __dirname + to public is what allowed me access to the images in assets/img folder
+app.use(express.static(__dirname + "/public"));
 
-// Parse application body
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
-
+//sets main.handlebars as the default layout and our view engine as handlebar
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
+//import routes and give the server access to them
 var routes = require("./controllers/burgerController.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("App now listening at localhost:" + PORT);
 });
