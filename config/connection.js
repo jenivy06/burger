2ff3cbd416fa0f36fac8b308b00aa799ae980c6f
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-06-24 08:26:17 -0600

    updated connection.js to connect to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "brandy978",
-  database: "burgers_db"
+  user: "com1hudvh1lgr996",
+  password: "m2uasnomwz47ep45",
+  database: "rkhzdj91iilbdphg"
 });
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'brandy978',
-    database: 'burgers_db'
+    host: 'axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'com1hudvh1lgr996',
+    password: 'm2uasnomwz47ep45',
+    database: 'rkhzdj91iilbdphg'
   })
 }
 
