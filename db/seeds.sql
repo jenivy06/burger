936ae070c9c2d4b3540b189fcbf2698611decd8d
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-06-23 18:14:19 -0600

    added font awesome to main.handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
     
 INSERT INTO burgers (burger_name) VALUES ('Cheeseburger'), ('Hamburger'), ('Veggie Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Steak burger', true), ('Mushroom Burger', false), ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
